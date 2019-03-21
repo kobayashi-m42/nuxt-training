26cbce95a77e11c5c21562acc7a8534541ee243f
kobayashi-m42 <m42kobayashi@gmail.com>
2019-03-21 16:57:44 +0900

    :sparkles: #3 レイアウトを活用したページを作成

---
+++ nuxt-api-examples/layouts/single.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>single layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <nuxt-link to="/">トップへ戻る</nuxt-link> <hr>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
