79cd8448c900597354c23f4a29848354937476a7
kobayashi-m42 <m42kobayashi@gmail.com>
2019-03-21 12:34:08 +0900

    :sparkles: #1 headメソッドでタイトルを設定

--- my-first-nuxt-app/pages/users/_id.vue
+++ my-first-nuxt-app/pages/users/_id.vue
@@ -22,6 +22,11 @@
 
 <script>
 export default {
+  head() {
+    return {
+      title: this.user.id
+    }
+  },
   async asyncData({ route, app }) {
     const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
     const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
