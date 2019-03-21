4f5578c32f72f19eae70802cb81abc797fb3ebe1
kobayashi-m42 <m42kobayashi@gmail.com>
2019-03-21 11:10:35 +0900

    :sparkles: #1 QiitaAPIから取得した情報をSSRする処理を追加

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -1,16 +1,53 @@
 <template>
-  <div>
-  <!-- -->
-  </div>
+  <section class="container">
+    <div>
+      <h3>Nuxt.js のタグが付けられた投稿の一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{item.title}} </span>
+            <small>
+              <span>by </span>
+              <nuxt-link :to="`/users/${item.user.id}`">
+                {{item.user.id}} </nuxt-link>
+            </small>
+          </h4>
+          <div>{{item.body.slice(0, 130)}}......</div>
+          <p><a :href="item.url">{{item.url}}</a></p>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  async mounted() {
-    console.log(
-      JSON.stringify(
-        await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
-    )
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   }
 }
 </script>
+
+<style>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p{
+  margin: 8px 0;
+}
+</style>
