c1eb99c8952c5133bf76ade73582df52e02a20e8
kobayashi-m42 <m42kobayashi@gmail.com>
2019-03-23 20:12:46 +0900

    :sparkles: #3 vuexのモジュールモードを実装

---
+++ nuxt-api-examples/store/users.js
@@ -0,0 +1,13 @@
+export const state = () => ({
+  list: []
+})
+export const mutations = {
+  addUser(state, user) {
+    state.list.push(user)
+  }
+}
+export const actions = {
+  addUser({ commit }, { user }) {
+    commit('addUser', user)
+  }
+}
