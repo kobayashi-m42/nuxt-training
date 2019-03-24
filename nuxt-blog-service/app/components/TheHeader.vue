3d74cd3be67da7d6657944a06288f6800b2739a8
kobayashi-m42 <m42kobayashi@gmail.com>
2019-03-24 09:31:17 +0900

    :sparkles: #5 ログインページと投稿一覧ページを仮実装

---
+++ nuxt-blog-service/app/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-menu mode="horizontal" :router="true">
+    <el-menu-item index="1" style="pointer-events:none;">
+      Nuxt Diary App
+    </el-menu-item>
+    <el-menu-item index="2" :route="{ path: '/posts/' }">
+      投稿一覧
+    </el-menu-item>
+    <no-ssr>
+      <el-menu-item index="4" style="float: right;" :route="{ path: '/' }">
+        <span>ログイン</span>
+      </el-menu-item>
+    </no-ssr>
+    <el-menu-item
+      index="5"
+      style="float: right"
+      :route="{ path: '/posts/new' }"
+    >
+      新規投稿
+    </el-menu-item>
+  </el-menu>
+</template>
--- nuxt-blog-service/app/pages/index.vue
+++ nuxt-blog-service/app/pages/index.vue
@@ -1,67 +1,45 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-blog-service
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <span>ログイン</span>
       </div>
-    </div>
+      <form>
+        <div class="form-content">
+          <span>ユーザー ID</span>
+          <el-input v-model="formData.id" placeholder="" />
+        </div>
+        <div class="form-content">
+          <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
+        </div>
+        <div class="text-right">
+          <el-button type="primary">{{ buttonText }}</el-button>
+        </div>
+      </form>
+    </el-card>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  computed: {
+    buttonText() {
+      return this.isCreateMode ? '新規登録' : 'ログイン'
+    }
+  },
+  asyncData() {
+    return {
+      isCreateMode: false,
+      formData: {
+        id: ''
+      }
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+.form-content {
+  margin: 16px 0;
 }
 </style>
